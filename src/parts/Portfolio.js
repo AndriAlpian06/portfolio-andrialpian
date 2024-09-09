a160137572e65f04cda337ab01669e93e4c9fc13
AndriAlpian06 <an.dri050695@gmail.com>
2024-09-09 09:14:20 +0700

    update skill & portfolio

--- src/parts/Portfolio.js
+++ src/parts/Portfolio.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import portfolio1 from '../images/portfolio-1.png'
-import portfolio2 from '../images/portfolio-2.png'
-import exprotates from '../images/exprotates.png'
-import gayeng from '../images/gayeng.png'
-import furnitueSecond from '../images/furniture-second.png'
-import wedding from '../images/wedding.png'
+import React from "react";
+import portfolio1 from "../images/portfolio-1.png";
+import portfolio2 from "../images/portfolio-2.png";
+import exprotates from "../images/exprotates.png";
+import gayeng from "../images/gayeng.png";
+import furnitueSecond from "../images/furniture-second.png";
+import wedding from "../images/wedding.png";
+import bestpoint from "../images/bestpoints.png";
 
 export default function Portfolio() {
   return (
-    <section id="portfolio" className='pt-36 pb-16 bg-white dark:bg-dark'>
-        <div className='container'>
+    <section id="portfolio" className="pt-28 pb-16 bg-white dark:bg-dark">
+      {/* <div className='container'>
             <div className='w-full px-4'>
                 <div className='max-w-xl mx-auto text-center mb-16'>
                     <h4 className='font-bold uppercase text-primary text-4xl mb-3'>Portfolio</h4>
@@ -33,45 +34,115 @@ export default function Portfolio() {
                     <p className='font-medium text-base text-black dark:text-white'>website PT GAYENG MAS ABADI adalah sebuah web company profile yang memberikan informasi seputar tentang videotron dan menawarkan videotron videotron di berbagai daerah untuk mengetahui selengkapnya berikut link web nya <a href="https://ex-gayengmasabadi.netlify.app/" target='_blank' rel="noreferrer" className='text-blue-500'>https://ex-gayengmasabadi.netlify.app/</a></p>
                 </div>
             </div>
+        </div> */}
+      <div className="container">
+        <div className="w-full px-4">
+          <div className="max-w-xl mx-auto text-center mb-16">
+            <h2 className="font-bold text-black dark:text-white text-3xl mb-4">
+              Project yang pernah di buat
+            </h2>
+            <p className="font-medium text-base text-black dark:text-white">
+              berikut project project saya yang saya buat dan develop dari
+              tempat saya berkerja
+            </p>
+          </div>
         </div>
-        <div className='container'>
-            <div className='w-full px-4'>
-                <div className='max-w-xl mx-auto text-center mb-16'>
-                    <h2 className='font-bold text-black dark:text-white text-3xl mb-4'>Project yang pernah di buat</h2>
-                    <p className='font-medium text-base text-black dark:text-white'>berikut project project saya yang saya buat dan develop dari tempat saya berkerja</p>
-                </div>
+        <div className="w-full px-4 flex flex-wrap justify-center">
+          <div className="mb-12 p-4 md:w-1/2">
+            <div className="rounded-md shadow-md overflow-hidden">
+              <img src={bestpoint} alt="" width="w-full" />
             </div>
-            <div className='w-full px-4 flex flex-wrap justify-center'>
-                <div className='mb-12 p-4 md:w-1/2'>
-                    <div className='rounded-md shadow-md overflow-hidden'>
-                        <img src={furnitueSecond} alt="" width="w-full"/>
-                    </div>
-                    <h3 className='font-semibold text-xl text-black dark:text-white mt-5 mb-3'>Website FURNITURE SECOND</h3>
-                    <p className='font-medium text-base text-black dark:text-white'>website FURNITURE SECOND adalah sebuah web penjualan barang barang mebel seperti kursi, meja, lemari dan brankas. dan juga menyediakan service kursi, meja dll untuk mengetahui selengkapnya berikut link web nya <a href="https://furniture-second.netlify.app/" target='_blank' rel="noreferrer" className='text-blue-500'>https://furniture-second.netlify.app/</a></p>
-                </div>
-                <div className='mb-12 p-4 md:w-1/2'>
-                    <div className='rounded-md shadow-md overflow-hidden'>
-                        <img src={wedding} alt="" width="w-full"/>
-                    </div>
-                    <h3 className='font-semibold text-xl text-black dark:text-white mt-5 mb-3'>Website WEDDING</h3>
-                    <p className='font-medium text-base text-black dark:text-white'>Website pernikahan sering kali menjadi pusat informasi bagi pasangan yang akan menikah dan tamu yang akan menghadiri perayaan tersebut. Biasanya, website pernikahan berisi informasi tentang acara, detail lokasi, waktu, dan daftar hadiah. <a href="https://ex-wedding.netlify.app/" target='_blank' rel="noreferrer" className='text-blue-500'>https://furniture-second.netlify.app/</a></p>
-                </div>
-                <div className='mb-12 p-4 md:w-1/2'>
-                    <div className='rounded-md shadow-md overflow-hidden'>
-                        <img src={portfolio1} alt="" width="w-full"/>
-                    </div>
-                    <h3 className='font-semibold text-xl text-black dark:text-white mt-5 mb-3'>Chatbot FB IndiHome Care</h3>
-                    <p className='font-medium text-base text-black dark:text-white'>chatbot ini saya buat untuk memberikan seputar informasi mengenai indihome ke pada customer, chatbot ini ada di channel fb messenger</p>
-                </div>
-                <div className='mb-12 p-4 md:w-1/2'>
-                    <div className='rounded-md shadow-md overflow-hidden'>
-                        <img src={portfolio2} alt="" width="w-full"/>
-                    </div>
-                    <h3 className='font-semibold text-xl text-black dark:text-white mt-5 mb-3'>Chatbot IndiHome.co.id</h3>
-                    <p className='font-medium text-base text-black dark:text-white'>chatbot ini saya buat untuk memberikan seputar informasi, permintaan dan panduan kendala mengenai indihome ke pada customer, chatbot ini ada di channel web indihome.co.id di halaman bantuan lalu pilih livechat</p>
-                </div>
+            <h3 className="font-semibold text-xl text-black dark:text-white mt-5 mb-3">
+              Website Bestpoints
+            </h3>
+            <p className="font-medium text-base text-black dark:text-white">
+              BestPoints adalah sebuah platform loyalty program yang
+              memungkinkan pengguna untuk mengumpulkan dan menukarkan poin dari
+              berbagai aktivitas. Dengan antarmuka yang user-friendly, website
+              ini menyediakan fitur pengelolaan akun, pengumpulan poin, serta
+              penukaran hadiah dengan mudah dan cepat{" "}
+              <a
+                href="https://bestpoints.andrialpian.my.id"
+                target="_blank"
+                rel="noreferrer"
+                className="text-blue-500"
+              >
+                https://bestpoints.andrialpian.my.id
+              </a>
+            </p>
+          </div>
+          <div className="mb-12 p-4 md:w-1/2">
+            <div className="rounded-md shadow-md overflow-hidden">
+              <img src={wedding} alt="" width="w-full" />
             </div>
+            <h3 className="font-semibold text-xl text-black dark:text-white mt-5 mb-3">
+              Website WEDDING
+            </h3>
+            <p className="font-medium text-base text-black dark:text-white">
+              Website pernikahan sering kali menjadi pusat informasi bagi
+              pasangan yang akan menikah dan tamu yang akan menghadiri perayaan
+              tersebut. Biasanya, website pernikahan berisi informasi tentang
+              acara, detail lokasi, waktu, dan daftar hadiah.{" "}
+              <a
+                href="https://ex-wedding.netlify.app/"
+                target="_blank"
+                rel="noreferrer"
+                className="text-blue-500"
+              >
+                https://furniture-second.netlify.app/
+              </a>
+            </p>
+          </div>
+          <div className="mb-12 p-4 md:w-1/2">
+            <div className="rounded-md shadow-md overflow-hidden">
+              <img src={furnitueSecond} alt="" width="w-full" />
+            </div>
+            <h3 className="font-semibold text-xl text-black dark:text-white mt-5 mb-3">
+              Website FURNITURE SECOND
+            </h3>
+            <p className="font-medium text-base text-black dark:text-white">
+              website FURNITURE SECOND adalah sebuah web penjualan barang barang
+              mebel seperti kursi, meja, lemari dan brankas. dan juga
+              menyediakan service kursi, meja dll untuk mengetahui selengkapnya
+              berikut link web nya{" "}
+              <a
+                href="https://furniture-second.netlify.app/"
+                target="_blank"
+                rel="noreferrer"
+                className="text-blue-500"
+              >
+                https://furniture-second.netlify.app/
+              </a>
+            </p>
+          </div>
+          <div className="mb-12 p-4 md:w-1/2">
+            <div className="rounded-md shadow-md overflow-hidden">
+              <img src={portfolio1} alt="" width="w-full" />
+            </div>
+            <h3 className="font-semibold text-xl text-black dark:text-white mt-5 mb-3">
+              Chatbot FB IndiHome Care
+            </h3>
+            <p className="font-medium text-base text-black dark:text-white">
+              chatbot ini saya buat untuk memberikan seputar informasi mengenai
+              indihome ke pada customer, chatbot ini ada di channel fb messenger
+            </p>
+          </div>
+          <div className="mb-12 p-4 md:w-1/2">
+            <div className="rounded-md shadow-md overflow-hidden">
+              <img src={portfolio2} alt="" width="w-full" />
+            </div>
+            <h3 className="font-semibold text-xl text-black dark:text-white mt-5 mb-3">
+              Chatbot IndiHome.co.id
+            </h3>
+            <p className="font-medium text-base text-black dark:text-white">
+              chatbot ini saya buat untuk memberikan seputar informasi,
+              permintaan dan panduan kendala mengenai indihome ke pada customer,
+              chatbot ini ada di channel web indihome.co.id di halaman bantuan
+              lalu pilih livechat
+            </p>
+          </div>
         </div>
+      </div>
     </section>
-  )
+  );
 }
--- src/parts/Skills.js
+++ src/parts/Skills.js
@@ -1,58 +1,89 @@
-import React from 'react'
+import React from "react";
 
-import HTML from '../images/skills/html.png';
-import CSS from '../images/skills/css.png';
-import JavaScript from '../images/skills/javascript.png';
-import ReactImg from '../images/skills/react.png';
-import Node from '../images/skills/node.png';
-import GitHub from '../images/skills/github.png';
-import Tailwind from '../images/skills/tailwind.png';
-import Mongo from '../images/skills/mongo.png';
+import HTML from "../images/skills/html.png";
+import CSS from "../images/skills/css.png";
+import JavaScript from "../images/skills/javascript.png";
+import ReactImg from "../images/skills/react.png";
+import Node from "../images/skills/node.png";
+import GitHub from "../images/skills/github.png";
+import Tailwind from "../images/skills/tailwind.png";
+import Mongo from "../images/skills/mongo.png";
+import Php from "../images/skills/php.png";
+import Python from "../images/skills/python.png";
+import Postgresql from "../images/skills/postgresql.png";
+import Bootstrap from "../images/skills/bootstrap.png";
 
 export default function Skills() {
   return (
-    <div name='skills' className='w-full h-screen bg-white dark:bg-dark text-white'>
+    <div
+      name="skills"
+      className="w-full h-screen bg-white dark:bg-dark text-white pt-28 pb-16"
+    >
       {/* Container */}
-      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-          <div className='max-w-xl mx-auto text-center'>
-              <p className='font-bold uppercase text-primary text-4xl inline border-b-4 border-pink-600'>Skills</p>
-              <p className='pey-2 font-medium text-base text-black dark:text-white'>Ini adalah teknologi yang pernah saya kerjakan</p>
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="max-w-xl mx-auto text-center">
+          <p className="font-bold uppercase text-primary text-4xl inline border-b-4 border-pink-600 px-4">
+            Skills
+          </p>
+          <p className="pey-2 font-medium text-base text-black dark:text-white pt-4">
+            Ini adalah teknologi yang pernah saya kerjakan
+          </p>
+        </div>
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-2">
+          <div className="shadow-md hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={HTML} alt="HTML icon" />
+            <p className="my-4 text-black dark:text-white">HTML</p>
           </div>
-          <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-2'>
-              <div className='shadow-md hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
-                  <p className='my-4 text-black dark:text-white'>HTML</p>
-              </div>
-              <div className='shadow-md hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={CSS} alt="HTML icon" />
-                  <p className='my-4 text-black dark:text-white'>CSS</p>
-              </div>
-              <div className='shadow-md hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={JavaScript} alt="HTML icon" />
-                  <p className='my-4 text-black dark:text-white'>JAVASCRIPT</p>
-              </div>
-              <div className='shadow-md hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={ReactImg} alt="HTML icon" />
-                  <p className='my-4 text-black dark:text-white'>REACT</p>
-              </div>
-              <div className='shadow-md hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto bg-black' src={GitHub} alt="HTML icon" />
-                  <p className='my-4 text-black dark:text-white'>GITHUB</p>
-              </div>
-              <div className='shadow-md hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Node} alt="HTML icon" />
-                  <p className='my-4 text-black dark:text-white'>NODE JS</p>
-              </div>
-              <div className='shadow-md hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Mongo} alt="HTML icon" />
-                  <p className='my-4 text-black dark:text-white'>MONGO DB</p>
-              </div>
-              <div className='shadow-md hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Tailwind} alt="HTML icon" />
-                  <p className='my-4 text-black dark:text-white'>Tailwind</p>
-              </div>
+          <div className="shadow-md hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={CSS} alt="HTML icon" />
+            <p className="my-4 text-black dark:text-white">CSS</p>
           </div>
+          <div className="shadow-md hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Php} alt="HTML icon" />
+            <p className="my-4 text-black dark:text-white">PHP</p>
+          </div>
+          <div className="shadow-md hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={JavaScript} alt="HTML icon" />
+            <p className="my-4 text-black dark:text-white">JAVASCRIPT</p>
+          </div>
+          <div className="shadow-md hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Python} alt="HTML icon" />
+            <p className="my-4 text-black dark:text-white">Python</p>
+          </div>
+          <div className="shadow-md hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={ReactImg} alt="HTML icon" />
+            <p className="my-4 text-black dark:text-white">REACT</p>
+          </div>
+          <div className="shadow-md hover:scale-110 duration-500">
+            <img
+              className="w-20 mx-auto bg-black"
+              src={GitHub}
+              alt="HTML icon"
+            />
+            <p className="my-4 text-black dark:text-white">GITHUB</p>
+          </div>
+          <div className="shadow-md hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Node} alt="HTML icon" />
+            <p className="my-4 text-black dark:text-white">NODE JS</p>
+          </div>
+          <div className="shadow-md hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Postgresql} alt="HTML icon" />
+            <p className="my-4 text-black dark:text-white">Postgresql</p>
+          </div>
+          <div className="shadow-md hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Mongo} alt="HTML icon" />
+            <p className="my-4 text-black dark:text-white">MONGO DB</p>
+          </div>
+          <div className="shadow-md hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Tailwind} alt="HTML icon" />
+            <p className="my-4 text-black dark:text-white">Tailwind</p>
+          </div>
+          <div className="shadow-md hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Bootstrap} alt="HTML icon" />
+            <p className="my-4 text-black dark:text-white">Bootstrap</p>
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
